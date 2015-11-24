5bde070bbd105ea414a5c613a9cb9832366133a8
XxrsfangxX <rsfang@NN123-HA-04.HS.sd203.org>
2015-11-24 14:10:05 -0600

    S
    dfa

---
+++ Chapter 7 practice/Array_practice/ArrayMethods.java
@@ -0,0 +1,73 @@
+
+
+/**
+ * Write a description of class ArrayMethods here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayMethods
+{
+    private int[] values;
+    public ArrayMethods(int[] initialValues){
+        values= initialValues;
+    }
+    public void swapFirstAndLast(){
+        int temp= values[0];
+        values[0]= values[(values.length)-1];
+        values[(values.length)-1]= temp;
+    }
+    public void shiftRight(){
+        int last=  values[(values.length)-1];
+        for (int i=0; i<values.length; i++){
+            values[i+1] = values[i];
+        }
+        values[0]= last;
+    
+    }
+    public void evensToZero(){
+        for( int i=0; i<values.length; i++){
+            if (i%2== 0){
+                values[i]=0;
+            }
+        }
+    }
+    
+    public void bigReplace(){
+        
+        for (int i=1; i<values.length-1; i++){
+            if( values[i-1]>values[i+1]){
+                values[i]= values[i-1];
+            }
+            else{
+                values[i]= values[i+1];
+            }
+        }
+    }
+    public void removeMiddle(){
+        
+       
+            if( (values.length)%2==0){
+           int [] newArray= new int[values.length-2];
+           for(int i=0;i <(values.length/2)-2; i++){
+               newArray[i]= values[i];
+            }
+                for(int i=0; i< (values.length/2)-1; i++){ 
+                newArray[((values.length+(i*2))/2)-1]= newArray[((values.length+1+i)/2)+1];
+           
+           }
+           newArray[values.length-2]= newArray[values.length-1];
+        }
+        else{
+            int[] newArray= new int[values.length-1];
+            for(int i=0;i <=(values.length/2)-1; i++){
+               newArray[i]= values[i];
+            }
+                for(int i=0; i< (values.length/2)-1; i++){ 
+                newArray[((values.length+(i*2))/2)-1]= newArray[((values.length+1+i)/2)+1];
+           
+           }
+        }
+    }
+}
+
