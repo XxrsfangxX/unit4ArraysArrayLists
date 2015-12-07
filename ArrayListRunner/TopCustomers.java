a2f2aca3e6188e386305dd356d033c5ee3c916fd
XxrsfangxX <rsfang@NN123-HA-04.HS.sd203.org>
2015-12-07 14:12:40 -0600

    p

--- ArrayListRunner/TopCustomers.java
+++ ArrayListRunner/TopCustomers.java
@@ -1,40 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 
-/**
- * Write a description of class TopCustomers here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class TopCustomers
 {
-    /** description of instance variable x (add comment for each instance variable) */
-    private int x;
+    private  ArrayList<String> names = new ArrayList<String>();
 
-    /**
-     * Default constructor for objects of class TopCustomers
-     */
-    public TopCustomers()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+    
+    public static void main( String [] args){
 
-    /**
-     * An example of a method - replace this comment with your own
-     *    that describes the operation of the method
-     *
-     * @pre        preconditions for the method
-     *            (what the method assumes about the method's parameters and class's state)
-     * @post    postconditions for the method
-     *            (what the method guarantees upon completion)
-     * @param    y    description of parameter y
-     * @return    description of the return value
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x+y;
+
     }
+    public  void addSale(String name, double amount){
+
+    }
+    public  String nameofBestCustomer(){
+
+     }
 
 }
